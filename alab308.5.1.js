5d2b88c67caf82c8fe6d1b744006f3ca62500b16
zhanar2024 <160571337+zhanar2024@users.noreply.github.com>
2024-05-29 19:36:03 -0400

    part3 added

--- alab308.5.1.js
+++ alab308.5.1.js
@@ -70,3 +70,34 @@ function printNumbers(n) {
 }
 const n = 10;
 printNumbers(n);
+
+//Part 2 -Thinking Methodically
+
+const data = [
+  { id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+  { id: "48", name: "Barry", occupation: "Runner", age: "25" },
+  { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+  { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+  { id: "7", name: "Bilbo", occupation: "None", age: "111" },
+];
+
+// Sorting the array by age
+const sortedByAge = data.sort((a, b) => parseInt(a.age) - parseInt(b.age));
+console.log(sortedByAge);
+
+// Filtering the array to remove entries with an age greater than 50
+const filteredByAge = data.filter((entry) => parseInt(entry.age) <= 50);
+
+console.log(filteredByAge);
+
+// Calculating the sum of the ages using the reduce method
+const sumOfAges = data.reduce((total, entry) => total + parseInt(entry.age), 0);
+
+console.log("Sum of ages:", sumOfAges);
+
+// Calculating the average age
+const averageAge = sumOfAges / data.length;
+
+console.log("Average age:", averageAge);
+
+//Part 3 - Thinking Critically
